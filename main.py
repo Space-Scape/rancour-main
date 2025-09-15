1e24db45b82cd9e243af1f22acb7a03cf905661b
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2025-09-14 20:57:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -490,7 +490,7 @@ class EventsView(View):
         self.add_item(RoleButton("Boss of the Week", get_emoji("botw")))
         self.add_item(RoleButton("Skill of the Week", get_emoji("sotw")))
         self.add_item(RoleButton("Sanguine Sunday - Learn ToB!", get_emoji("sanguine_sunday")))
-        self.add_item(RoleButton("PvP", get_emoji("💀")))
+        self.add_item(RoleButton("PvP", get_emoji("skull")))
 
 # ---------------------------
 # 🔹 RSN Commands
